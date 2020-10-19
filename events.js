36e265b6f405d83ded6d98e1c8f2cc00a2f77edf
jim <jim@jpmiii.com>
2020-10-19 09:36:12 -0500

    more big changes

---
+++ events.js
@@ -0,0 +1,167 @@
+
+
+
+
+on("ready", function() {
+    if( ! state.MainGameNS ) {
+        state.MainGameNS = { index: 0, dis: 0 };
+    }
+    log(getHarnTimeStr(state.MainGameNS.GameTime));
+	log("loaded. trace: "+ trace);
+	initializeTables(0);
+	started = true;
+});
+
+
+
+
+
+
+on("chat:message", function(msg) {
+if (trace) {log(`>chat:message(${msg.content})`);}
+ if(msg.type == "api") {
+	var args = msg.content.split(" ");
+	if (dispatch_table.hasOwnProperty(args[0])) {
+		var commandMap = dispatch_table[args[0]];
+		try {
+			// First, test the syntax regular expression if it has one
+			if (commandMap.hasOwnProperty("re_syntax")) {
+				log(`comparing syntax to ${commandMap.re_syntax}`)
+				if (!commandMap.re_syntax.test(msg.content)) {
+					var errorMessage = `&{template:default} {{name=Syntax error}} {{Received=${msg.content}}}`;
+					if (commandMap.hasOwnProperty("hr_syntax")) {
+						errorMessage += `{{Expected=${commandMap.hr_syntax}}}`;
+					}
+					sendChat("API", errorMessage);
+					return;
+				}
+			}
+			// syntax check passed or doesn't exist. Execute the action if it exists.
+			if (commandMap.hasOwnProperty("action")) {
+				commandMap.action(args, msg)
+			} else {
+				sendChat("API Error", `No action defined for ${args[0]}`)
+			}
+		} catch (err) {
+			// Something went wrong. Log it, alert in chat and prevent the sandbox from bailing.
+			log(err.stack);
+			sendChat("API Error", err.message);
+		}
+	} else {
+		log(`No such command ${msg.content}`);
+		sendChat("API Error", `&{template:default} {{name=Unknown command}} {{Received=${msg.content}}}`)
+
+	}
+
+ } else {
+
+	// check for and log crits
+	if (msg.content.startsWith(" {{character_name=")) {
+		var d = new Date();
+		var n = d.toLocaleString();
+		if (msg.content.includes("rolldesc=rolls ")) {
+			if (msg.inlinerolls[3].results.total % 5 == 0) {
+				var char = getCharByNameAtt(msg.content.slice((msg.content.indexOf("character_name")+15),msg.content.indexOf("}} ")));
+
+				if (msg.inlinerolls[1].results.total >= msg.inlinerolls[3].results.total) {
+					charLog(char.id,  ": CS " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=rolls ")+15,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=rolls "))),realtime,gametime)
+				} else {
+					charLog(char.id,  ": CF " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=rolls ")+15,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=rolls "))),realtime,gametime)
+				}
+			}
+		} else 	if (msg.content.includes("rolldesc=performs ")) {
+			if (msg.inlinerolls[7].results.total % 5 == 0) {
+				var char = getCharByNameAtt(msg.content.slice((msg.content.indexOf("character_name")+15),msg.content.indexOf("}} ")));
+
+				if (msg.inlinerolls[4].results.total >= msg.inlinerolls[7].results.total) {
+					charLog(char.id,  ": CS " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=performs ")+18,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=performs "))),realtime,gametime)
+				} else {
+					charLog(char.id,  ": CF " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=performs ")+18,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=performs "))),realtime,gametime )
+				}
+			}
+		} else 	if (msg.content.includes("rolldesc=casts ")) {
+			if (msg.inlinerolls[7].results.total % 5 == 0) {
+				var char = getCharByNameAtt(msg.content.slice((msg.content.indexOf("character_name")+15),msg.content.indexOf("}} ")));
+
+				if (msg.inlinerolls[4].results.total >= msg.inlinerolls[7].results.total) {
+					charLog(char.id,  ": CS " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=casts ")+15,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=casts "))),realtime,gametime)
+				} else {
+					charLog(char.id,  ": CF " 
+						+ msg.content.slice(msg.content.indexOf("rolldesc=casts ")+15,
+						msg.content.indexOf("}} ", msg.content.indexOf("rolldesc=casts "))),realtime,gametime )
+				}
+			}
+		}
+	}
+ }
+ if (trace) {log("<chat:message")}
+});
+
+
+
+
+
+
+
+on("change:attribute:current", function(obj, prev) {
+
+    if((obj.get('name') == "INITIATIVE_ML") || (obj.get('name') == "UNIVERSAL_PENALTY") || (obj.get('name') == "ENCUMBRANCE") || (obj.get('name') == "RIDER")){
+
+        charid = obj.get("_characterid");
+
+    	var rideratt = findObjs({
+    		name: 'RIDER',
+    		_type: "attribute",
+    		_characterid: charid,
+    	})[0];
+        if (rideratt) {
+        	var rider = findObjs({
+        		name: rideratt.get('current'),
+        		_type: "character",
+        	})[0];
+        	if (rider) {
+            	mySet('STEED_INIT',rider.id,(parseInt(myGet('INITIATIVE_ML', charid, 0)) - (parseInt(myGet('UNIVERSAL_PENALTY', charid, 0)) * 5)- (parseInt(myGet('ENCUMBRANCE', charid, 0)) * 5)));
+
+        	}
+        }
+    } else if (obj.get('name').includes("WEAPON_NAME") && weapon_list_on) {
+        setWeaponsList(obj.get("_characterid"));
+    } else if (obj.get('name').includes("SKILL_NAME") && skill_list_on) {
+        setSkillList(obj.get("_characterid"));
+    } else if (obj.get('name') == "tab") {
+		if (obj.get('current') == 2 && skill_list_on) {
+	        setSkillList(obj.get("_characterid"));
+		} else if (obj.get('current') == 3 && weapon_list_on) {
+	        setWeaponsList(obj.get("_characterid"));
+		}
+    }
+});
+
+on("change:campaign:turnorder", function(obj, prev) {
+
+    if(Campaign().get("turnorder") !== "") {
+        if(prev["turnorder"] !== "") {
+            var turnorder = JSON.parse(Campaign().get("turnorder"));
+            var oldturnorder = JSON.parse(prev["turnorder"]);
+            if (turnorder.length > 0) {
+                if (turnorder[0]["id"] !== oldturnorder[0]["id"]) {
+                    toke = getObj("graphic",turnorder[0]["id"]);
+                    sendGMPing(toke.get('left'), toke.get('top'), toke.get('pageid'), "", true);
+                }
+            }
+        }
+    }
+
+});
+
+
---
+++ handle_defend.js
@@ -0,0 +1,477 @@
+
+
+
+function handle_defend(def, msg) {
+
+	var atk = state.MainGameNS.attacker;
+	var wepname = state.MainGameNS.wepname;
+	var atoke = getObj("graphic", atk[1]);
+
+	var toke = getObj("graphic", atk[6]);
+	if (!toke.get("represents")) {sendChat(msg.who, "No defender"); return;}
+	if (!toke.get("represents").startsWith("-M")) {sendChat(msg.who, "No defender -M"); return;}
+	var charid = atoke.get("represents")
+	var defcharid = toke.get("represents")
+	var defchar = getObj("character", defcharid);
+	var aspect = 0;
+	var allowed = defchar.get("controlledby");
+    if(!playerIsGM(msg.playerid)) {
+        if (allowed.indexOf(msg.playerid) == -1 &&  allowed.indexOf("all") == -1){
+            senChat("API", msg.who + " is not in control")
+            return;
+        }
+    }
+
+
+	var wep = filterObjs(function(obj) {
+		obn = obj.get('name');
+		if (obn) {
+			if ((obn.indexOf("WEAPON_NAME")) !== -1
+					&& (obj.get("_characterid") == charid)
+					&& (obj.get("current") == wepname)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	});
+	if (!wep) {
+		senChat("API", msg.who + " No weapon")
+		return;
+	}
+
+	var aojn = wep[0].get('name');
+
+	if (atk[3] == "H") {
+
+		var baspect = myGet(aojn.slice(0, -4) + "B", charid, 0)
+		var easpect = myGet(aojn.slice(0, -4) + "E", charid, 0)
+		var paspect = myGet(aojn.slice(0, -4) + "P", charid, 0)
+
+		if (baspect !== "-") {
+			aspect = parseInt(baspect);
+			var aspstr = "B";
+		}
+		if (easpect !== "-") {
+			if (parseInt(easpect) >= aspect) {
+				aspect = parseInt(easpect);
+				var aspstr = "E";
+			}
+		}
+		if (paspect !== "-") {
+			if (parseInt(paspect) >= aspect) {
+				aspect = parseInt(paspect);
+				var aspstr = "P";
+			}
+		}
+	} else {
+		var aspect = parseInt(myGet(aojn.slice(0, -4) + atk[3], charid, 0));
+		var aspstr = atk[3];
+	}
+	if (atk[4] == "missile") {
+	    if (wepname in missile_range) {
+	        aspect = state.MainGameNS.missi[1];
+	    } else {
+	        aspect = Math.round(aspect * parseFloat(state.MainGameNS.missi[1]))
+	    }
+	}
+
+	if (toke.get('bar3_value')) {
+
+		var pp = (parseInt(toke.get('bar3_value')) + parseInt(myGet(
+				'ENCUMBRANCE', defcharid, 0))) * 5;
+
+	} else {
+		var pp = (parseInt(myGet('UNIVERSAL_PENALTY', defcharid, 0)) + parseInt(myGet(
+				'ENCUMBRANCE', defcharid, 0))) * 5;
+	}
+
+	if (def[1] == "dodge") {
+	    var defml = 0;
+	    if ((atk[4] == "missile") && (wepname.indexOf("Bow") !== -1)) {
+		    defml = Math.round(parseInt(myGet("DODGE_ML", defcharid, 0))/2) + parseInt(def[2])- (pp);
+	    } else {
+	        defml = parseInt(myGet("DODGE_ML", defcharid, 0)) + parseInt(def[2])- (pp);
+	    };
+
+		var defstr = "";// "+" +  parseInt(myGet("DODGE_ML", defcharid, 0)) + "+" + parseInt(def[2])+ "-" + (pp);
+		var defwepname = "Dodge";
+
+	}
+
+	if ((def[1] == "block") || (def[1] == "counterstrike")) {
+		var defwepname = msg.content.slice((msg.content.indexOf("WeaponName:") + 11));
+
+		if (defwepname.length > 3) {
+
+			var defwep = filterObjs(function(obj) {
+				obn = obj.get('name');
+				if (obn) {
+					if ((obn.indexOf("WEAPON_NAME")) !== -1
+							&& (obj.get("_characterid") == defcharid)
+							&& (obj.get("current") == defwepname)) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			});
+
+			var ojn = defwep[0].get('name');
+
+			if (def[1] == "counterstrike") {
+			    var defml = parseInt(myGet(ojn.slice(0, -4) + "ML", defcharid, 0)) - pp + parseInt(myGet(ojn.slice(0, -4) + "ATK", defcharid, 0)) + parseInt(myGet(ojn.slice(0, -4) + "HM", defcharid, 0)) + parseInt(def[2]);
+
+			} else {
+			    var defml = parseInt(myGet(ojn.slice(0, -4) + "ML", defcharid, 0)) - pp + parseInt(myGet(ojn.slice(0, -4) + "DEF", defcharid, 0)) + parseInt(myGet(ojn.slice(0, -4) + "HM", defcharid, 0)) + parseInt(def[2]);
+
+			}
+
+		} else {
+			def[1] = "ignore";
+			var defstr = ""
+		}
+
+	}
+
+	droll = randomInteger(100);
+
+	var ctype = parseInt(myGet('DCType', defcharid, 0))
+
+	log("Def roll: " + droll);
+	if (ctype !== 0) {
+	    for (i=0;i<(ctype*-1);i++) {
+	        var broll = randomInteger(300);
+	        log("cheat: " + aroll + " " +broll);
+	        if (broll < droll) {
+	            droll=broll
+	        }
+	    }
+	    if (ctype > 0) {
+	        droll = 101 - droll;
+	    }
+	    log("Cheat Def roll: " + droll);
+
+	}
+
+	
+	log("defml: "+defml)
+	
+
+	if (state.MainGameNS["cheat"] > 0) {
+	    if (state.MainGameNS["cheat"] >100) {
+	        droll = 100 - state.MainGameNS["cheat"]
+	    } else {
+	        droll = state.MainGameNS["cheat"]
+	        state.MainGameNS["cheat"] = 0;
+	    }
+
+	}
+
+	if (defml >95) {defml=95;}
+
+	if ((droll <= defml) && (droll % 5 == 0)) {
+		var dsuc = "CS";
+		var dis = 3;
+	}
+	if ((droll <= defml) && (droll % 5 !== 0)) {
+		var dsuc = "MS";
+		var dis = 2;
+	}
+	if ((droll > defml) && (droll % 5 !== 0)) {
+		var dsuc = "MF";
+		var dis = 1;
+	}
+	if ((droll > defml) && (droll % 5 == 0)) {
+		var dsuc = "CF";
+		var dis = 0;
+	}
+
+	if (def[1] == "ignore") {
+		dis = 0;
+	}
+
+	if (atk[4] == "missile") {
+
+		var r = attack_missile[def[1]][state.MainGameNS.ais][dis];
+
+	} else {
+		var r = attack_melee[def[1]][state.MainGameNS.ais][dis];
+	}
+
+
+	var res = r + "<br/>";
+
+	if ((r.indexOf("A*") == 0) || (r.indexOf("B*") == 0)
+			|| (r.indexOf("M*") == 0)) {
+
+
+		var impactroll = "";
+		var tot = 0;
+
+		var sides = 6;
+		var bm = 0;
+
+		var imd = "";//myGet(ojn.slice(0, -4) + "NOTE", charid, "")
+		var attribute = findObjs({
+    		type: 'attribute',
+    		characterid: charid,
+    		name: aojn.slice(0, -4) + "NOTE"
+	    })[0]
+
+	    if (attribute) {
+	        imd = attribute.get('current');
+
+	    }
+	    log("imd: " + imd);
+
+		if (imd.length>0) {
+    		var impactmod = imd.split(":")
+
+    		if (impactmod.length==2) {
+    		    aspect = aspect + parseInt(impactmod[1]);
+    		    log("Impact mod: "+impactmod[1]);
+    		}
+    		if (impactmod.length==3) {
+    		    log("Impact mod: "+impactmod[1] + " : " + impactmod[2]);
+    		    if(impactmod[1] == "d") {
+
+    		        for (i = 0; i < parseInt(impactmod[2]); i++) {
+            			var ir = randomInteger(6);
+            			tot = tot + ir;
+            			if (impactroll.length > 2) {
+            				impactroll = impactroll + " + " + ir;
+
+            			} else {
+            				impactroll = impactroll + "[[" + ir;
+
+            			}
+    		        }
+    		    } else {
+    		    sides =  parseInt(impactmod[1]);
+    		    bm =  parseInt(impactmod[2]);
+    		    }
+    		}
+		}
+
+		for (i = 0; i < parseInt(r.slice(2)); i++) {
+			var ir = randomInteger(sides) + bm;
+			tot = tot + ir
+			if (impactroll.length > 2) {
+				impactroll = impactroll + " + " + ir;
+
+			} else {
+				impactroll = impactroll + "[[" + ir;
+
+			}
+		}
+
+
+		tot = tot + aspect;
+		impactroll = impactroll + " + " + aspect + " ]]";
+
+
+		var hitloc = gethitloc(randomInteger(100), hit_loc_penalty[atk[2]]["index"]);
+
+		var avatloc = myGet(hitloc + "_" + aspstr, defcharid,0);
+
+		res = res + "<br/>Attacker "+atoke.get('name')+" Impact: " + impactroll + "<br/>Location: "
+				+ hitloc + "<br/>AV at Loc: " + avatloc
+				+ "<br/>Effective Impact: " + (tot - avatloc);
+		if (tot - avatloc > 0) {
+			var eff = gethiteff(hitloc, tot - avatloc);
+			res = res + "<br/>Defender Injury: " + eff + " " + aspstr
+			var unipenalty =  parseInt(eff.match(/\d/));
+			if (toke.get('bar3_link')) {
+
+				var unipenalty = unipenalty + parseInt(myGet('UNIVERSAL_PENALTY', defcharid, 0));
+				toke.set('bar3_value', unipenalty);
+				addinjury(aspstr+" "+hitloc, eff, defcharid)
+			} else if (toke.get('bar3_value')){
+				var unipenalty = unipenalty + parseInt(toke.get('bar3_value'));
+				toke.set('bar3_value', unipenalty);
+			} else {
+				var unipenalty = unipenalty + parseInt(myGet('UNIVERSAL_PENALTY', defcharid, 0));
+				toke.set('bar3_value', unipenalty);
+			}
+
+			res = res + rollshock(defcharid, toke, unipenalty)
+
+		}
+
+	}
+
+	if ((r.indexOf("D*") == 0) || (r.indexOf("B*") == 0)) {
+
+		var baspect = myGet(ojn.slice(0, -4) + "B", defcharid, 0)
+		var easpect = myGet(ojn.slice(0, -4) + "E", defcharid, 0)
+		var paspect = myGet(ojn.slice(0, -4) + "P", defcharid, 0)
+		var defaspstr = "";
+		if (baspect !== "-") {
+			aspect = parseInt(baspect);
+			var defaspstr = "B";
+		}
+		if (easpect !== "-") {
+			if (parseInt(easpect) >= aspect) {
+				aspect = parseInt(easpect);
+				var defaspstr = "E";
+			}
+		}
+		if (paspect !== "-") {
+			if (parseInt(paspect) >= aspect) {
+				aspect = parseInt(paspect);
+				var defaspstr = "P";
+			}
+		}
+
+
+		var impactroll = "";
+		var tot = 0;
+
+		var sides = 6;
+		var bm = 0;
+		var imd = "";//myGet(ojn.slice(0, -4) + "NOTE", charid, "")
+		var attribute = findObjs({
+    		type: 'attribute',
+    		characterid: defcharid,
+    		name: aojn.slice(0, -4) + "NOTE"
+	    })[0]
+
+	    if (attribute) {
+	        imd = attribute.get('current');
+
+	    }
+		if (imd.length>0) {
+    		var impactmod = imd.split(":")
+    		if (impactmod.length==2) {
+    		    aspect = aspect + parseInt(impactmod[1]);
+    		    log("Impact mod: "+impactmod[1]);
+    		}
+    		if (impactmod.length==3) {
+    		    log("Impact mod: "+impactmod[1] + " : " + impactmod[2]);
+    		    if(impactmod[1] == "d") {
+
+    		        for (i = 0; i < parseInt(impactmod[2]); i++) {
+            			var ir = randomInteger(6);
+            			tot = tot + ir;
+            			if (impactroll.length > 2) {
+            				impactroll = impactroll + " + " + ir;
+
+            			} else {
+            				impactroll = impactroll + "[[" + ir;
+
+            			}
+    		        }
+    		    } else {
+    		    sides =  parseInt(impactmod[1]);
+    		    bm =  parseInt(impactmod[2]);
+    		    }
+    		}
+		}
+
+		for (i = 0; i < parseInt(r.slice(2)); i++) {
+			var ir = randomInteger(sides) + bm;
+			tot = tot + ir
+			if (impactroll.length > 2) {
+				impactroll = impactroll + " + " + ir;
+
+			} else {
+				impactroll = impactroll + "[[" + ir;
+
+			}
+		}
+
+		tot = tot + aspect;
+		impactroll = impactroll + " + " + aspect + " ]]";
+
+		var hitloc = gethitloc(randomInteger(100), 1);
+
+		var avatloc = myGet(hitloc + "_" + defaspstr, charid,0);
+
+		res = res + "<br/>Counterstriker "+toke.get('name')+" Impact: " + impactroll + "<br/>Location: "
+				+ hitloc + "<br/>AV at Loc: " + avatloc
+				+ "<br/>Effective Impact: " + (tot - avatloc);
+		if (tot - avatloc > 0) {
+			var eff = gethiteff(hitloc, tot - avatloc);
+			res = res + "<br/>Attacker Injury: " + eff + " " + defaspstr
+			var unipenalty =  parseInt(eff.match(/\d/));
+			if (atoke.get('bar3_link')) {
+
+				var unipenalty = unipenalty + parseInt(myGet('UNIVERSAL_PENALTY', charid, 0));
+				atoke.set('bar3_value', unipenalty);
+				addinjury(defaspstr+" "+hitloc, eff, charid)
+			} else if (atoke.get('bar3_value')){
+				var unipenalty = unipenalty + parseInt(atoke.get('bar3_value'));
+				atoke.set('bar3_value', unipenalty);
+			} else {
+				var unipenalty = unipenalty + parseInt(myGet('UNIVERSAL_PENALTY', charid, 0));
+				atoke.set('bar3_value', unipenalty);
+			}
+			res = res + rollshock(charid, atoke, unipenalty)
+		}
+	}
+
+	if (def[1] == "dodge") {
+
+	    if ((atk[4] == "missile") && (wepname.indexOf("Bow") !== -1)) {
+            var drolltarg = parseInt(parseInt(myGet("DODGE_ML", defcharid, 0))/2) + "[1/2ML] -" + (pp) + "[PP] +" +parseInt(def[2]) + "[Sit]";
+        } else {
+            var drolltarg = parseInt(myGet("DODGE_ML", defcharid, 0)) + "[ML] -" + (pp) + "[PP] +" +parseInt(def[2]) + "[Sit]";
+        }
+
+		var defstr = "&{template:harnroll} {{rolldesc=" + toke.get('name') + " attempts dodge}} {{rollresult=[["
+		        +  state.MainGameNS.aroll + "]]}} {{rolltarget=[[" + state.MainGameNS.appstr + "]]}} {{rollsuccess=[["	+ state.MainGameNS.ais + "]]}} {{drollresult=[[" +  droll + "]]}} {{drolltarget=[["
+		        + drolltarg+ "]]}}{{drollsuccess=[["	+ dis + "]]}} {{result=" + res + "}}";
+	} else if (def[1] == "ignore") {
+
+
+		var defstr = "&{template:harnroll} {{rolldesc=" + toke.get('name') + " ignores}} {{rollresult=[["
+		        +  state.MainGameNS.aroll + "]]}} {{rolltarget=[[" + state.MainGameNS.appstr + "]]}} {{rollsuccess=[["	+ state.MainGameNS.ais + "]]}} {{result=" + res + "}}";
+	} else {
+		var notestr =   + state.MainGameNS.atkstrout + "<br><h4>"
+				+ toke.get('name') + " " + def[1] + "s with a "
+				+ defwepname + "</h4><br/>Penalty: " + pp
+				+ "<br/>Other mods: "+ def[2] + "<br>Mastery Level: "	+ (parseInt(myGet(ojn.slice(0, -4) + "ML", defcharid, 0)) + parseInt(myGet(ojn.slice(0, -4) + "DEF", defcharid, 0)))
+				+ "<h4>Result:</h4>" + res
+
+		if (def[1] =="counterstrike") {
+		    var drolltarg = myGet(ojn.slice(0, -4) + "ML", defcharid, 0) + "[ML] +" + myGet(ojn.slice(0, -4) + "ATK", defcharid, 0) + "[Atk] +" + myGet(ojn.slice(0, -4) + "HM", defcharid, 0) + "[HM] -" +  pp + "[PP] +" + def[2] + "[Sit]";
+
+		} else {
+		    var drolltarg = myGet(ojn.slice(0, -4) + "ML", defcharid, 0) + "[ML] +" + myGet(ojn.slice(0, -4) + "DEF", defcharid, 0) + "[Def] +" + myGet(ojn.slice(0, -4) + "HM", defcharid, 0) + "[HM] -" +  pp + "[PP] +" + def[2] + "[Sit]";
+
+		}
+
+		var defstr = "&{template:harnroll} {{rolldesc=" + toke.get('name') + " " + def[1] + "s with a " + defwepname + "}} {{rollresult=[["
+		        +  state.MainGameNS.aroll + "]]}} {{rolltarget=[[" + state.MainGameNS.appstr + "]]}} {{rollsuccess=[["	+ state.MainGameNS.ais + "]]}} {{drollresult=[[" +  droll + "]]}} {{drolltarget=[["
+		        + drolltarg+ "]]}}{{drollsuccess=[["+ dis + "]]}} {{result=" + res + "}}";
+
+	}
+	//log crits
+	
+
+	if (state.MainGameNS.asuc == "CS") {
+
+		charLog(charid, ": Attack CS " + wepname,realtime,gametime)
+	} else if (state.MainGameNS.asuc == "CF") {
+
+		charLog(charid, ": Attack CF " + wepname,realtime,gametime)
+	} 
+	if (dsuc == "CS") {
+
+		charLog(charid, ": Defend CS " + defwepname,realtime,gametime)
+	} else if (dsuc == "CF") {
+
+		charLog(charid, ": Defend CF " + defwepname,realtime,gametime)
+	} 
+
+	sendChat(msg.who, defstr);
+
+}
+
+
+
