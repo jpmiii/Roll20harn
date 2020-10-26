d66d65b7cc424d7a4a5370c00712f3697faf8b07
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-10-25 20:56:43 -0600

    Corrected variable name, set defaults to canon.

--- config.js
+++ config.js
@@ -16,26 +16,26 @@ var defend_template = "harn-defend"; // sets the roll template (harn-defend or h
 // House rules - default should always be canon rules
 //
 
-var missle_close_range_mod = -15; // house rule close range mod (0 is canon) adds an extra range column for close vs short range
-var randomize_init_roll = true; // adds 3d6 to the init for randomness
-var emlmax = 97; // canon 95
-var emlmin = 4; // canon 5
+var missle_close_range_mod = 0; // house rule close range mod (0 is canon) adds an extra range column for close vs short range
+var randomize_init_roll = false; // adds 3d6 to the init for randomness
+var emlmax = 95; // canon 95
+var emlmin = 5; // canon 5
 var additional_target_locations = false; //add additional target locations e.g. neck for faster combat resolution
 
 // house rule tables - comment out to remove. Any entry listed here which is also in the canon table will override the canon table.
-var house_rule_skilllist = {
-    "Needlework": {"type":"LORE","sba":["DEX","EYE","WIL"],"ssm":{"Ula":"0"},"oml":"1","notes":""}
-};
-var house_rule_prices = {
-    "Bow(Minicrossbow)":{"price":1800,"weight":2.5},
-    "Lightsabre":{"price":9600000,"weight":0.2}
-};
-var hosue_rule_occupations = {
-    "Archeologist": [
-        "Gun/3",
-        "Whip/3"
-    ]
-}
-var house_rule_occupation_time = {
-    "Archeologist": "14"
-}
+// var house_rule_skilllist = {
+//     "Needlework": {"type":"LORE","sba":["DEX","EYE","WIL"],"ssm":{"Ula":"0"},"oml":"1","notes":""}
+// };
+// var house_rule_prices = {
+//     "Bow(Minicrossbow)":{"price":1800,"weight":2.5},
+//     "Lightsabre":{"price":9600000,"weight":0.2}
+// };
+// var house_rule_occupational_skills = {
+//     "Archeologist": [
+//         "Gun/3",
+//         "Whip/3"
+//     ]
+// }
+// var house_rule_occupation_time = {
+//     "Archeologist": "14"
+// }
