a050571f5a2592861af281718841366b3e9b096b
Stephen Cooper <scooper4711@users.noreply.github.com>
2020-10-25 20:25:37 -0600

    Create config object for settings and house rules.
    This commit creates a config object with version management
    which is used for holding configuration and house rules.
    
    The config object has the ability to add additional
    items for inventory, skills, occupations etc.
    
    Closes #36

--- events.js
+++ events.js
@@ -6,8 +6,20 @@ on("ready", function() {
 	if (!state.MainGameNS) {
 		state.MainGameNS = { index: 0, dis: 0 };
 	}
+	expectedSerial = 1;
+	if (config.serial != expectedSerial) {
+		sendChat("API", `Unexpected config serial number. Expected ${expectedSerial} but got ${config.serial}`);
+	}
 	log(getHarnTimeStr(state.MainGameNS.GameTime));
-	log("loaded. trace: " + trace);
+	log("trace: " + trace);
+	house_remove(config.remove_items, prices, "inventory");
+	house_remove(config.remove_armor_coverage, armor_coverage, "armor coverage");
+	house_add(config.add_skills, skilllist, "skill");
+	house_add(config.add_items, prices, "inventory");
+	house_add(config.add_armor_coverage, armor_coverage, "skill");
+	house_add(config.add_armor_prot, armor_prot, "skill");
+	house_add(config.add_occupational_skills, occupational_skills, "occupation skill");
+	house_add(config.add_occupation_time, occupation_time, "occupation time");
 	initializeTables(0);
 	started = true;
 });
@@ -90,12 +102,12 @@ on("change:attribute:current", function(obj, prev) {
 		}
 	} else if (obj.get('name').includes("WEAPON_NAME") && weapon_list_on) {
 		setWeaponsList(obj.get("_characterid"));
-	} else if (obj.get('name').includes("SKILL_NAME") && skill_list_on) {
+	} else if (obj.get('name').includes("SKILL_NAME") && config.skill_list_on) {
 		setSkillList(obj.get("_characterid"));
 	} else if (obj.get('name') == "tab") {
-		if (obj.get('current') == 2 && skill_list_on) {
+		if (obj.get('current') == 2 && config.skill_list_on) {
 			setSkillList(obj.get("_characterid"));
-		} else if (obj.get('current') == 3 && weapon_list_on) {
+		} else if (obj.get('current') == 3 && config.weapon_list_on) {
 			setWeaponsList(obj.get("_characterid"));
 		}
 	}
@@ -117,4 +129,20 @@ on("change:campaign:turnorder", function(obj, prev) {
 	}
 
 });
+
+function house_remove(house_remove, canon, description) {
+	house_remove.forEach((k) => {
+		if (trace)
+			log(`Removing ${description} ${k}`);
+		delete canon[k];
+	});
+}
+
+function house_add(house_add, canon, description) {
+	Object.keys(house_add).forEach((k) => {
+		canon[k] = house_add[k];
+		if (trace)
+			log(`Adding house rule ${description}: ${k}`);
+	});
+}
 
