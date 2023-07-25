988c5d4dd3a46f072556ecb705eabb0142446ec7
Jim Mccall <git@jpmiii.com>
2023-07-24 22:25:14 -0500

    fixed error in skill improve

--- untitled.js
+++ untitled.js
@@ -821,15 +821,20 @@ function handle_pickskill(args, msg) {
 
 function handle_improveskill(args, msg) {
 	char = getObj("character", args[1]);
+	var inc = 1;
 	if (parseInt(args[2])) {
 	    skill_att_name = findSkill(char, msg.content.slice(35+args[2].length));
+	    	    
 	    loop = parseInt(args[2]);
-	    
+	    if (loop < 0) {
+	        loop = loop * -1;
+	        inc = 2;
+	    } 
 	} else {
 	    skill_att_name = findSkill(char, msg.content.slice(34));
 	    loop = 1;
 	}
-	if (loop > 100) {loop = 100;}
+	if (loop>36) {loop=36;}
 	var d = new Date();
 	var n = d.toLocaleString();
 	var ml = parseInt(myGet(skill_att_name.slice(0, -4) + "ML", char.id, 0));
@@ -837,15 +842,16 @@ function handle_improveskill(args, msg) {
 
     	roll = randomInteger(100) + parseInt(myGet(skill_att_name.slice(0, -4) + "SB", char.id, 0));
     	if (roll >= ml) {
-    		mySet(skill_att_name.slice(0, -4) + "ML", char.id, (ml + 1));
+    	    ml=ml+inc;
+    		mySet(skill_att_name.slice(0, -4) + "ML", char.id, (ml ));
     		if ((loop-1) == i) {
     		    sendChat("Skill Improvement " + myGet("NAME", char.id, ""), "<br>"
-    			+ "<br>" + " roll " + roll + ": SUCCESS<br>" + msg.content.slice(34) + " ML increases to " + (ml + 1));    		    
+    			+ "<br>" + " roll " + roll + ": SUCCESS<br>" + msg.content.slice(34) + " ML increases to " + (ml ));    		    
     		}
 
     		charLog(char.id, ": Skill Improvement Roll: " + msg.content.slice(34) + " "
-    			+ roll + ": SUCCESS: ML = " + (ml + 1), config.realtime, config.gametime);
-    		ml=ml+1;
+    			+ roll + ": SUCCESS: ML = " + ml, config.realtime, config.gametime);
+    		
     	} else {
     	    if ((loop-1) == i) {
     	        sendChat("Skill Improvement " + myGet("NAME", char.id, ""), "<br>" + msg.content.slice(34)
